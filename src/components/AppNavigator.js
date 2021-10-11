403fc2bdbbd6c0c5151c1e8a3ec77de0094d78cd
rugieri <rugieri.ronaldo@gmail.com>
2021-10-11 20:05:07 -0300

    create navigation bar

--- src/components/AppNavigator.js
+++ src/components/AppNavigator.js
@@ -1,5 +1,30 @@
 import React from 'react';
+import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
+import { Link } from 'react-router-dom';
+
+const useStyles = makeStyles((themes) => ({
+  AppBar: {
+    backgroundColor: 'black',
+  },
+  link: {
+    textDecoration: 'none',
+  },
+  title: {
+    cursor: 'pointer',
+    color: 'white',
+  },
+}));
 
 export default function AppNavigator() {
-  return <div></div>;
+  const classes = useStyles();
+  return (
+    <AppBar className={classes.AppBar} position="fixed" variant="h6">
+      <Toolbar>
+        <Link to="/" className={classes.link}>
+          <Typography className={classes.title}>Pokedex</Typography>
+        </Link>
+      </Toolbar>
+    </AppBar>
+  );
 }
