d9f2fa616f59ec71276fb7378ccee16a58dba29a
rugieri <rugieri.ronaldo@gmail.com>
2021-10-12 14:45:45 -0300

    Using the pokemon API to get the pokedex data

--- src/containers/Pokedex.js
+++ src/containers/Pokedex.js
@@ -1,7 +1,38 @@
-import React, { useEffect } from 'react';
-import { Box } from '@material-ui/core';
+/* eslint-disable no-undef */
+import React, { useEffect, useState } from 'react';
+import { Box, CircularProgress } from '@material-ui/core';
+import axios from 'axios';
+import { IMAGE_API_URL, POKEMON_API_URL } from '../config';
 
 export default function Pokedex() {
-  useEffect(() => {}, []);
-  return <Box></Box>;
+  const [pokemonData, setPokemonData] = useState([null]);
+  useEffect(() => {
+    axios.get(POKEMON_API_URL + '?limit=800').then((response) => {
+      if (response.status >= 200 && response.status < 300) {
+        const { results } = response.data;
+        let newPokemonData = [];
+        results.forEach((pokemon, index) => {
+          index++;
+          let pokemonObject = {
+            id: index,
+            url: IMAGE_API_URL + index + '.png',
+            name: pokemon.name,
+          };
+          newPokemonData.push(pokemonObject);
+        });
+        setPokemonData(newPokemonData);
+      }
+    });
+  }, []);
+  return (
+    <Box>
+      {pokemonData ? (
+        pokemonData.map((pokemon) => {
+          return <h1>{pokemon.name}</h1>;
+        })
+      ) : (
+        <CircularProgress style={{ marginTop: 100 }} />
+      )}
+    </Box>
+  );
 }
